4a6ac3a4aa73bd74d58168a452e790ca2507a204
Llydia Cross <llydiacross@gmail.com>
2023-01-27 13:11:23 +0000

    fixed infinity mint

---
+++ infinitymint.config.ts
@@ -0,0 +1,28 @@
+import { InfinityMintConfig } from "infinitymint/dist/app/interfaces";
+
+//please visit docs.infinitymint.app for a more complete starter configuration file
+const config: InfinityMintConfig = {
+  console: true,
+  hardhat: {
+    solidity: {
+      version: "0.8.12",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 20,
+        },
+      },
+    },
+    paths: {
+      tests: "./tests",
+    },
+  },
+  roots: ["../infinitymint-buildtools/"],
+  settings: {
+    networks: {},
+    scripts: {
+      disableJavascriptRequire: ["/infinitymint-buildtools/"],
+    },
+  },
+};
+export default config;
