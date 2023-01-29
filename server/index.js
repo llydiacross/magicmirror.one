63503703a8ceb68c1f926fc624720d0d6881efbd
koma <m@kae.si>
2023-01-29 15:21:35 +0000

    FIx: Use npm for package scripts

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 const port = 9090;
@@ -8,9 +9,15 @@ server.use(cors({
   origin: 'http://localhost:3000',
 }));
 
+server.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
+
 server.get('/', (_request, response) => {
   response.send('Hello World!')
 });
+
+server.get('/api/ai/generate', (_request, repsonse) => {})
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
