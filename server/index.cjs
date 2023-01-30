f3c8fe74bbf88ab57babe9ab10092e8efdcd7943
Llydia Cross <llydiacross@gmail.com>
2023-01-30 04:54:03 +0000

    f

--- server/index.cjs
+++ server/index.cjs
@@ -1,53 +1,75 @@
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const { Configuration, OpenAIApi } = require("openai")
-const bodyParser = require("body-parser")
-const morgan = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const { Configuration, OpenAIApi } = require("openai");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
-const server = express()
-const port = 9090
-require("dotenv").config()
+const server = express();
+const port = 9090;
+require("dotenv").config();
 
 // Configure OpenAI
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY
-})
+  apiKey: process.env.OPENAI_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(morgan("dev"))
+//function like an api
+server.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-server.use(cors({
-  origin: ["http://localhost:3000", "https://web.infinitymint.app","https://webx.infinitymint.app"]
-}))
+// whitelist for CORS
+var whitelist = ["http://localhost", "https://webx.infinitymint.app", "https://web.infinitymint.app"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+server.use(cors(corsOptions));
 
-server.use(helmet({
-  crossOriginResourcePolicy: false
-}))
+//the json body parser
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
+//for dev
+server.use(morgan("dev"));
+
+server.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
 // An error handling middleware
 server.use((err, _request, response, _next) => {
-  response.status(500).send("Oops, something went wrong.\n", err)
-})
+  response.status(500).send("Oops, something went wrong.\n", err);
+});
 
 server.get("/", (_request, response) => {
-  response.status(200).json({ok: true})
-})
+  response.status(200).json({ ok: true });
+});
 
 server.post("/gpt/prompt", async (request, response) => {
-  
-  let temperature = parseFloat(request.body.temp) || 0.6
-  if (isNaN(temperature)) temperature = 0.6
+  let temperature = parseFloat(request.body.temp) || 0.6;
+  if (isNaN(temperature)) temperature = 0.6;
 
-  if (temperature > 3) temperature = 3
+  if (temperature > 3) temperature = 3;
 
-  let n = parseInt(request.body.n) || 2
-  if (isNaN(n)) n = 2
+  let n = parseInt(request.body.n) || 2;
+  if (isNaN(n)) n = 2;
 
-  if (n > 6) n = 6
+  if (n > 6) n = 6;
 
   try {
     const completion = await openai.createCompletion({
@@ -58,14 +80,14 @@ server.post("/gpt/prompt", async (request, response) => {
       max_tokens: 2048,
       top_p: 1,
       frequency_penalty: 0,
-      presence_penalty: 0
-    })
-  response.status(200).send(completion.data)
+      presence_penalty: 0,
+    });
+    response.status(200).send(completion.data);
   } catch (error) {
-    throw new Error("Something went horribly wrong!", error.status)
+    throw new Error("Something went horribly wrong!", error.status);
   }
-})
+});
 
 server.listen(port, () => {
-  console.log(`web3.eth listening on port ${port}`)
-})
+  console.log(`web3.eth listening on port ${port}`);
+});
