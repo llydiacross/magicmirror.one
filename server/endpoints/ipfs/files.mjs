1bec155f26adef5afd243b159cafb53db13c9ecd
Llydia Cross <llydiacross@gmail.com>
2023-04-05 17:43:31 +0100

    cInitializing Web3

--- server/endpoints/ipfs/files.mjs
+++ server/endpoints/ipfs/files.mjs
@@ -1,5 +1,5 @@
-import { success } from '../../utils/helpers.mjs'
-import server from '../../server.mjs'
+import { success } from '../../utils/helpers.mjs';
+import server from '../../server.mjs';
 
 /**
  *
@@ -7,38 +7,39 @@ import server from '../../server.mjs'
  * @param {import('express').Response} res
  */
 export const post = async (req, res) => {
-  const cid = req.body.cid
-  const links = []
+  const cid = req.body.cid;
+  const links = [];
   for await (const link of server.ipfs.ls(cid)) {
-    if (links.length > 32) break
+    if (links.length > 32) break;
 
     if (link.type === 'file') {
-      const stats = await server.ipfs.object.stat(link.path)
-      link.size = stats.CumulativeSize
+      const stats = await server.ipfs.object.stat(link.path);
+      link.size = stats.CumulativeSize;
 
-      if (!link.name) continue
+      if (!link.name) continue;
 
-      const extension = link.name.split('.').pop()
+      const extension = link.name.split('.').pop();
 
-      if (!server?.config?.allowedExtensions?.includes(extension)) continue
+      if (!server?.config?.allowedExtensions?.includes(extension)) continue;
 
       if (link.size < 1024 * 1024 * 10) {
-        const resp = server.ipfs.cat(link.path)
-        let content = []
+        const resp = server.ipfs.cat(link.path);
+        let content = [];
         for await (const chunk of resp) {
-          content = [...content, ...chunk]
+          content = [...content, ...chunk];
         }
-        link.content = content
+        link.content = content;
       }
-      links.push(link)
-      continue
+      links.push(link);
+      continue;
     }
 
     // is dir
-    links.push(link)
+    links.push(link);
   }
+
   success(res, {
     cid,
-    files: links
-  })
-}
+    files: links,
+  });
+};
