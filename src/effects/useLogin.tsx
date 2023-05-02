41bac95a8e255baec11e30928c971be1df6e08bb
Llydia Cross <llydiacross@gmail.com>
2023-05-03 00:29:52 +0100

    fg

--- src/effects/useLogin.tsx
+++ src/effects/useLogin.tsx
@@ -1,64 +1,70 @@
-import { ethers } from 'ethers'
-import { SiweMessage } from 'siwe'
-import { useState } from 'react'
-import { useAuthContext } from './useAuthContext'
+import { ethers } from 'ethers';
+import { SiweMessage } from 'siwe';
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+import { getEndpointHref } from '../api';
 
-const domain = window.location.host
+const domain = window.location.host;
 
-async function createSiweMessage (address: any, statement: string): Promise<string> {
-  const response = await fetch('http://localhost:9090/wallet/nonce', {
-    credentials: 'include'
-  })
-  const message = new SiweMessage({
-    domain,
-    address,
-    statement,
-    uri: origin,
-    version: '1',
-    chainId: 1,
-    nonce: await response.text()
-  })
+async function createSiweMessage(
+	address: any,
+	statement: string
+): Promise<string> {
+	const response = await fetch(getEndpointHref() + 'nonce', {
+		credentials: 'include',
+	});
+	const message = new SiweMessage({
+		domain,
+		address,
+		statement,
+		uri: origin,
+		version: '1',
+		chainId: 1,
+		nonce: await response.text(),
+	});
 
-  return message.prepareMessage()
+	return message.prepareMessage();
 }
 
 export const useLogin = () => {
-  const [error, setError] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
-  const signer = provider.getSigner()
-  const { state, dispatch } = useAuthContext()
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const provider = new ethers.providers.Web3Provider(window.ethereum);
+	const signer = provider.getSigner();
+	const { state, dispatch } = useAuthContext();
 
-  const login = async wallet => {
-    setIsLoading(true)
+	const login = async (wallet) => {
+		setIsLoading(true);
+		setError(null);
 
-    const message = await createSiweMessage(
-      await state.walletAddress,
-      'Sign in with Ethereum to the app.'
-    )
-    const signature = await signer.signMessage(message)
+		const message = await createSiweMessage(
+			await state.walletAddress,
+			'Sign in with Ethereum to the app.'
+		);
+		const signature = await signer.signMessage(message);
 
-    const response = await fetch('http://localhost:9090/wallet/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ message, signature }),
-      credentials: 'include'
-    })
+		const response = await fetch(getEndpointHref() + 'login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ message, signature }),
+			credentials: 'include',
+		});
 
-    console.log(response)
+		console.log(response);
 
-    if (!response.ok) {
-      setIsLoading(false)
-      setError(response.body)
-    }
+		if (!response.ok) {
+			setIsLoading(false);
+			setError(response.body);
+			return;
+		}
 
-    if (response.status === 200) {
-      dispatch({ type: 'LOGIN', payload: response })
-      setIsLoading(false)
-    }
-  }
+		if (response.status === 200) {
+			dispatch({ type: 'LOGIN', payload: response });
+			setIsLoading(false);
+		}
+	};
 
-  return { login, isLoading, error }
-}
+	return { login, isLoading, error };
+};
