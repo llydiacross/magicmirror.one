299e4c795f942cc14466e939d03dcb7575e65c6d
Llydia Cross <llydiacross@gmail.com>
2023-05-02 21:01:07 +0100

    fix

--- server/endpoints/wallet/nonce.mjs
+++ server/endpoints/wallet/nonce.mjs
@@ -1,6 +1,13 @@
-import { generateNonce } from 'siwe'
+import { generateNonce } from 'siwe';
+import { userError } from '../../utils/helpers.mjs';
 export const get = (request, response) => {
-  request.session.nonce = generateNonce()
-  response.setHeader('Content-Type', 'text/plain')
-  response.status(200).send(request.session.nonce)
-}
+	if (request.session.siwe)
+		return userError(
+			res,
+			'You are already logged in. Please log out to generate a new nonce to login with.'
+		);
+
+	request.session.nonce = generateNonce();
+	response.setHeader('Content-Type', 'text/plain');
+	response.status(200).send(request.session.nonce);
+};
