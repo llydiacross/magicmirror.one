401f7e1894e344d4c8b8486d082b2518c94c5e80
Llydia Cross <llydiacross@gmail.com>
2023-05-03 08:40:05 +0100

    f

--- server/endpoints/ipns/resolve.mjs
+++ server/endpoints/ipns/resolve.mjs
@@ -7,29 +7,29 @@ import { userError } from '../../utils/helpers.mjs';
  * @param {import('express').Response} res
  */
 export const post = async (req, res) => {
-  let name = req.body.path;
+	let name = req.body.path;
 
-  if (!name.includes('/ipns/')) name = '/ipns/' + name;
+	if (!name.includes('/ipns/')) name = '/ipns/' + name;
 
-  if (!name) {
-    return res.status(400).send({
-      error: 'Missing name',
-    });
-  }
+	if (!name) {
+		return res.status(400).send({
+			error: 'Missing name',
+		});
+	}
 
-  const result = await server.ipfs.name.resolve(name, {
-    timeout: 2000,
-  });
+	const result = await server.ipfs.name.resolve(name, {
+		timeout: 2000,
+	});
 
-  let path;
-  for await (const name of result) {
-    path = name;
-  }
+	let path;
+	for await (const name of result) {
+		console.log(path);
+	}
 
-  if (path === undefined)
-    return userError(res, 'eth address has bad IPNS resolution');
+	if (path === undefined)
+		return userError(res, 'eth address has bad IPNS resolution');
 
-  res.json({
-    cid: path,
-  });
+	res.json({
+		cid: path,
+	});
 };
