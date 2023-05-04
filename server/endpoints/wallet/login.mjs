f14e6c3a340fb2afb762619a05dcf2659e248618
Llydia Cross <llydiacross@gmail.com>
2023-05-04 03:06:15 +0100

    cleaned up kaes code

--- server/endpoints/wallet/login.mjs
+++ server/endpoints/wallet/login.mjs
@@ -1,6 +1,7 @@
 import pkg from 'siwe';
 import jwt from 'jsonwebtoken';
 import server from '../../server.mjs';
+import { userError } from '../../utils/helpers.mjs';
 const { SiweMessage, ErrorTypes } = pkg;
 
 /**
@@ -9,93 +10,66 @@ const { SiweMessage, ErrorTypes } = pkg;
  * @param {import('express').Response} response
  */
 export const post = async (request, response) => {
-	const { method } = request;
-	switch (method) {
-		default: // If anything other than a POST, deny it.
-			response.setHeader('Allow', ['POST']);
-			response.status(405).end(`Method ${method} Not Allowed`);
-			break;
-		case 'POST':
-			try {
-				if (!request.body.message) {
-					response.status(422).json({
-						ok: false,
-						error: 'Expected prepareMessage object as body.',
-					});
-					return;
-				}
+	try {
+		if (!request.body.message) return userError(response, 'Missing message.');
 
-				const { message, signature } = request.body;
-				const siweMessage = new SiweMessage(message);
-				const fields = await siweMessage.validate(signature);
+		const { message, signature } = request.body;
+		const siweMessage = new SiweMessage(message);
+		const fields = await siweMessage.validate(signature);
 
-				if (fields.nonce !== request.session.nonce) {
-					response.status(422).json({
-						ok: false,
-						error: 'Invalid nonce.',
-					});
-					return;
-				}
+		if (fields.nonce !== request.session.nonce) {
+			request.session.siwe = null;
+			request.session.nonce = null;
+			await new Promise((resolve) => request.session.save(resolve));
+			return userError(response, 'Invalid nonce.');
+		}
 
-				request.session.siwe = fields;
-				request.session.cookie.expires = new Date(fields.expirationTime);
+		request.session.siwe = fields;
+		request.session.cookie.expires = new Date(fields.expirationTime);
 
-				// We'll use this for actually authenticating the user to login.
-				const jwtToken = jwt.sign({ message }, process.env.JWT_KEY, {
-					expiresIn: '12h',
-				});
+		// We'll use this for actually authenticating the user to login.
+		const jwtToken = jwt.sign({ message }, process.env.JWT_KEY, {
+			expiresIn: '12h',
+		});
 
-				// Set the wallet's sessionID to redis.
-				server.redisClient.set(fields.address, request.sessionID);
-				request.session.save(() =>
-					response
-						.status(200)
-						.cookie('jwt_token=authentication', jwtToken)
-						.send(jwtToken)
-						.end()
-				);
+		// Set the wallet's sessionID to redis.
+		server.redisClient.set(fields.address, request.sessionID);
+		request.session.save(() =>
+			response
+				.status(200)
+				.cookie('jwt_token=authentication', jwtToken)
+				.send(jwtToken)
+				.end()
+		);
 
-				//add them to the database if they don't exist
-				if (
-					server.prisma.user.count({
-						where: {
-							address: fields.address,
-						},
-					}) === 0
-				)
-					server.prisma.user.create({
-						data: {
-							address: fields.address,
-						},
-					});
-			} catch (err) {
-				request.session.siwe = null;
-				request.session.nonce = null;
-				console.error(err);
+		//add them to the database if they don't exist
+		if (
+			server.prisma.user.count({
+				where: {
+					address: fields.address,
+				},
+			}) === 0
+		)
+			server.prisma.user.create({
+				data: {
+					address: fields.address,
+				},
+			});
+	} catch (err) {
+		request.session.siwe = null;
+		request.session.nonce = null;
+		await new Promise((resolve) => request.session.save(resolve));
+		// Log the error.
+		console.error(err);
+		// Very specifc error handling.
+		switch (err) {
+			case ErrorTypes.EXPIRED_MESSAGE:
+				return userError(response, 'Expired message.');
 
-				// Very specifc error handling.
-				const { message } = err;
-				switch (err) {
-					case ErrorTypes.EXPIRED_MESSAGE: {
-						request.session.save(() =>
-							response.status(440).ok(false).json({ message })
-						);
-						break;
-					}
-					case ErrorTypes.INVALID_SIGNATURE: {
-						request.session.save(() =>
-							response.status(422).ok(false).json({ message })
-						);
-						break;
-					}
-					default: {
-						request.session.save(() =>
-							response.status(500).ok(false).json({ message })
-						);
-						break;
-					}
-				}
-			}
-			break;
+			case ErrorTypes.INVALID_SIGNATURE:
+				return userError(response, 'Invalid signature.');
+			default:
+				return userError(response, err.message);
+		}
 	}
 };
