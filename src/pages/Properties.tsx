91c5b0a1ae5c9d8db723721356c69ec461d3bb87
Llydia Cross <llydiacross@gmail.com>
2023-05-04 13:44:05 +0100

    g

--- src/pages/Properties.tsx
+++ src/pages/Properties.tsx
@@ -299,7 +299,7 @@ export default function Properties() {
 													}
 													className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 													onClick={() => {
-														getAllEns()
+														fetchENS()
 															.catch((error) => {
 																setError(error);
 															})
@@ -310,7 +310,7 @@ export default function Properties() {
 															});
 													}}
 												>
-													Refresh
+													Fetch
 												</button>
 											</div>
 										</div>
