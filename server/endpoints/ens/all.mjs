90afdd030236bf99144345faa6a8b73b47b6bcd9
Llydia Cross <llydiacross@gmail.com>
2023-05-08 10:40:13 +0100

    added check every 5 minutes if user is still logged in

--- server/endpoints/ens/all.mjs
+++ server/endpoints/ens/all.mjs
@@ -7,21 +7,26 @@ import { ethers } from 'ethers';
  * @param {import('express').Response} res
  */
 export const get = async (req, res) => {
-	let { address, page } = req.query;
+	let { address, page, justDomainNames } = req.query;
 	page = parseInt(page) || 0;
+	justDomainNames = justDomainNames === 'true';
 
 	if (!address) return userError(res, 'Missing address');
 	if (!ethers.utils.isAddress(address))
 		return userError(res, 'Invalid address');
 
-	let enses = await server.prisma.eNS.findMany({
+	let statement = {
 		where: {
 			ownerAddress: address,
 		},
-
 		skip: page * server.config.magicMirror.pageMax,
 		take: server.config.magicMirror.pageMax,
-	});
+	};
+	if (justDomainNames)
+		statement.select = {
+			domainName: true,
+		};
+	let enses = await server.prisma.eNS.findMany(statement);
 
 	if (enses)
 		enses = enses.map((ens) => {
