f542df5a3f09c9a281df82ab18c2cca5ca6e7865
koma <m@kae.si>
2023-05-12 02:03:35 +0100

    f

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -173,11 +173,9 @@ export default function Leaderboard() {
 																<td className="px-6 py-4 whitespace-nowrap">
 																	<div className="text-sm text-gray-900">
 																		<p>
-																			{index ===
-																				0 &&
-																				' 🏆 '}
-																			{index +
-																				1}
+																			{index + 1}
+																			{index === 0
+																				&& ' 🏆 '}
 																		</p>
 																	</div>
 																</td>
@@ -257,11 +255,9 @@ export default function Leaderboard() {
 																<td className="px-6 py-4 whitespace-nowrap">
 																	<div className="text-sm text-gray-900">
 																		<p>
-																			{index ===
-																				0 &&
-																				' 🏆 '}
-																			{index +
-																				1}
+																			{index + 1}
+																			{index === 0
+																				&& ' 🏆 '}
 																		</p>
 																	</div>
 																</td>
