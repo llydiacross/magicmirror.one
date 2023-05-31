accf42a01ba438f380874ff9d71a405e7ae32106
Llydia Cross <llydiacross@gmail.com>
2023-05-31 18:29:23 +0100

    f

--- infinitymint.config.ts
+++ infinitymint.config.ts
@@ -166,4 +166,48 @@ const config: InfinityMintConfig = {
 	export: {},
 };
 
+(config as any).magicMirror = {
+	pageMax: 100,
+	/**
+	 * @type {import('ipfs-core').Options}
+	 */
+	ipfs: {},
+	/**
+	 * If non set, will look for the environment variable OPENAI_KEY to use as the api key
+	 * @type {import('openai').Configuration}
+	 */
+	openapi: {},
+
+	allowedExtensions: [
+		'xens',
+		'html',
+		'js',
+		'css',
+		'json',
+		'htm',
+		'svg',
+		'partial',
+		'mp3',
+		'wav',
+		'flac',
+	],
+	/**
+	 * used to get files from IPFS, can be set to local host to use the node that is inside of the server already
+	 */
+	ipfsEndpoint: 'https://ipfs.io/api/v0/',
+	/**
+	 * CORS allowed origins, can be set to an empty array or removed to allow all origins
+	 */
+	cors: [
+		'http://localhost:3000',
+		'https://webx.infinitymint.app',
+		'https://magicmirror.one',
+		'https://reflect.magicmirror.one',
+		'https://geocities.com',
+		'https://infinitymint.app',
+		'https://web.infinitymint.app',
+		'https://web-api.infinitymint.app',
+	],
+};
+
 export default config;
