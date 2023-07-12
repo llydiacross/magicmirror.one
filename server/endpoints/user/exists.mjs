2132ba0ba1e1f5e86bd8f5962509f3335a56964d
Llydia Cross <llydiacross@gmail.com>
2023-07-12 07:29:18 +0100

    f

--- server/endpoints/user/exists.mjs
+++ server/endpoints/user/exists.mjs
@@ -1,34 +1,36 @@
-import server from '../../server.mjs'
-import { userError } from '../../utils/helpers.mjs'
-import { ethers } from 'ethers'
+import server from '../../server.mjs';
+import { userError } from '../../utils/helpers.mjs';
+import { ethers } from 'ethers';
 
 export const settings = {
 	requireLogin: false,
-	admin: false
-}
+	admin: false,
+};
 
 export const get = async (req, res) => {
-	await post(req, res)
-}
+	await post(req, res);
+};
 
 export const post = async (req, res) => {
-	let {address} = req.query
+	let { address } = req.query;
 
 	if (!address || address === null || address === undefined) {
-		return userError(res, 'No address provided in field!')
+		return userError(res, 'No address provided in field!');
 	}
 
 	if (!ethers.utils.isAddress(address)) {
 		return userError(res, 'Invalid address');
 	}
 
-	let validUser = await server.prisma.user.findUnique(
-		{where: {address}}
-	)
+	let validUser = await server.prisma.user.findUnique({ where: { address } });
 
 	if (!validUser) {
-		return false
+		return {
+			exists: false,
+		};
 	}
 
-	return true
-}
+	return {
+		exists: true,
+	};
+};
