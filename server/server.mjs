783724d2cd5dba155edcc8f859e783ad1725805f
llydia <llydiacross@gmail.com>
2024-10-20 20:44:02 +0100

    fuck

--- server/server.mjs
+++ server/server.mjs
@@ -1,31 +1,31 @@
-import express from 'express';
-import glue from 'jsglue';
-import { findEndpoints, getEndpointPath, userError } from './utils/helpers.mjs';
-import cors from 'cors';
-import helmet from 'helmet';
-import bodyParser from 'body-parser';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import Session from 'express-session';
-import { create } from 'ipfs-http-client';
-import { createClient } from 'redis';
-import { Alchemy, Network } from 'alchemy-sdk';
+import express from "express";
+import glue from "jsglue";
+import { findEndpoints, getEndpointPath, userError } from "./utils/helpers.mjs";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import Session from "express-session";
+import { create } from "ipfs-http-client";
+import { createClient } from "redis";
+import { Alchemy, Network } from "alchemy-sdk";
 //prisma
-import { PrismaClient } from '@prisma/client';
-import tsNode from 'ts-node';
-import { Configuration, OpenAIApi } from 'openai';
-import { isLoggedIn } from './utils/helpers.mjs';
+import { PrismaClient } from "@prisma/client";
+import tsNode from "ts-node";
+import { Configuration, OpenAIApi } from "openai";
+import { isLoggedIn } from "./utils/helpers.mjs";
 //yargs
-import yargs from 'yargs/yargs';
-import { hideBin } from 'yargs/helpers';
-import { logDirect } from 'infinitymint/dist/app/helpers.js';
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import { logDirect } from "infinitymint/dist/app/helpers.js";
 const argv = yargs(hideBin(process.argv)).argv;
 
 // do ts node register
 tsNode.register({
 	transpileOnly: true,
 	compilerOptions: {
-		module: 'commonjs',
+		module: "commonjs",
 	},
 });
 
@@ -89,22 +89,22 @@ export class Server {
 	constructor(port = 9090) {
 		if (!process.env.SIWE_SECRET)
 			throw new Error(
-				'SIWE_SECRET not set in .env. Please make sure to set it. It can equal any random base64 string.'
+				"SIWE_SECRET not set in .env. Please make sure to set it. It can equal any random base64 string.",
 			);
 
 		if (!process.env.ALCHEMY_API_KEY)
 			throw new Error(
-				'ALCHEMY_API_KEY not set in .env. Please set your alchemy API key in the .env and start again.'
+				"ALCHEMY_API_KEY not set in .env. Please set your alchemy API key in the .env and start again.",
 			);
 
 		if (!process.env.OPENAI_API_KEY)
 			throw new Error(
-				'OPENAI_API_KEY not set in .env. Please set your openai API key in the .env and start again.'
+				"OPENAI_API_KEY not set in .env. Please set your openai API key in the .env and start again.",
 			);
 
 		if (!process.env.DATABASE_URL)
 			throw new Error(
-				'DATABASE_URL not set in .env. Please set your postgress database url in the .env and start again.'
+				"DATABASE_URL not set in .env. Please set your postgress database url in the .env and start again.",
 			);
 
 		this.app = express();
@@ -115,10 +115,10 @@ export class Server {
 
 		this.app.use((_, res, next) => {
 			res.header(
-				'Access-Control-Allow-Headers',
-				'Origin, X-Requested-With, Content-Type, Accept'
+				"Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept",
 			);
-			res.header('Access-Control-Allow-Credentials', 'true');
+			res.header("Access-Control-Allow-Credentials", "true");
 			next();
 		});
 
@@ -127,17 +127,17 @@ export class Server {
 		this.app.use(bodyParser.urlencoded({ extended: true }));
 
 		// for dev
-		this.app.use(morgan('dev'));
+		this.app.use(morgan("dev"));
 
 		// Integrating SIWE
 		this.app.use(
 			Session({
-				name: 'magicmirror',
+				name: "magicmirror",
 				secret: process.env.SIWE_SECRET,
 				resave: true,
 				saveUninitialized: true,
 				cookie: { secure: false, sameSite: true },
-			})
+			}),
 		);
 	}
 
@@ -151,23 +151,20 @@ export class Server {
 		/**
 		 * @type {import('infinitymint')}
 		 */
-		let infinityMint = await wrapper.getSync('infinitymint');
+		let infinityMint = await wrapper.getSync("infinitymint");
 		this.infinityConsole = await infinityMint.load({
 			dontDraw: true,
 			scriptMode: true,
 			startExpress: false,
 			startGanache: false,
-			network: process.env.PRODUCTION === 'true' ? 'ethereum' : 'ganache',
+			network: process.env.PRODUCTION === "true" ? "ethereum" : "ganache",
 			//will tell InfinityMint not to pipe its outputs to the InfinityConsole and instead print them directly to the console, essentially showing all of the logs! turning this to false will only show debug and default logs
 			dontPipe: true,
 		});
 		this.config = this.infinityConsole.Helpers.getConfigFile();
 		this.ipfs = create({
 			port: 5050,
-			url:
-				this.config.magicMirror.ipfsEndpoint ||
-				'https://ipfs.io/ipfs/',
-			
+			url: this.config.magicMirror.ipfsEndpoint || "https://ipfs.io/ipfs/",
 		});
 		this.alchemy = new Alchemy({
 			apiKey: process.env.ALCHEMY_API_KEY,
@@ -178,12 +175,12 @@ export class Server {
 		this.openAI = new OpenAIApi(
 			new Configuration(
 				this?.config?.magicMirror?.openapi &&
-				this?.config?.magicMirror?.openapi?.apiKey
+					this?.config?.magicMirror?.openapi?.apiKey
 					? this.config.magicMirror.openapi
 					: {
 							apiKey: process.env.OPENAI_API_KEY,
-					  }
-			)
+						},
+			),
 		);
 
 		// set CORS after config has been loaded
@@ -193,8 +190,8 @@ export class Server {
 					this.config.magicMirror.cors &&
 					this.config.magicMirror.cors.length !== 0
 						? this.config.magicMirror.cors
-						: '*',
-			})
+						: "*",
+			}),
 		);
 
 		//wait for redis to connect
@@ -203,12 +200,12 @@ export class Server {
 		await this.loadEndpoints();
 
 		// handle redis errors
-		this.redisClient.on('err', (err) => {
+		this.redisClient.on("err", (err) => {
 			console.error(err.stack);
 			process.exit(129);
 		});
 
-		this.app.get('/', (_, res) => {
+		this.app.get("/", (_, res) => {
 			res.send(`
 			<h1>MagicMirror API</h1>
 			<p>Server is running</p>
@@ -220,14 +217,12 @@ export class Server {
 					.map(
 						(route) =>
 							`<li><a href="${route.path}">${route.path}</a> ${
-								route?.settings?.requireLogin
-									? '(Requires Login)'
-									: ''
-							} ${route?.settings?.admin ? '(Admin Only)' : ''} ${
-								route.get ? '[GET] ' : ''
-							} ${route.post ? '[POST] ' : ''}</li>`
+								route?.settings?.requireLogin ? "(Requires Login)" : ""
+							} ${route?.settings?.admin ? "(Admin Only)" : ""} ${
+								route.get ? "[GET] " : ""
+							} ${route.post ? "[POST] " : ""}</li>`,
 					)
-					.join('')}
+					.join("")}
 			</ul>
 			`);
 
@@ -256,16 +251,16 @@ export class Server {
 				let path =
 					route.path ||
 					file
-						.replace(endpointPath, '')
-						.replace('.mjs', '')
-						.replace('.js', '')
-						.replace(process.cwd(), '');
+						.replace(endpointPath, "")
+						.replace(".mjs", "")
+						.replace(".js", "")
+						.replace(process.cwd(), "");
 
-				if (path[0] !== '/') path = '/' + path;
+				if (path[0] !== "/") path = "/" + path;
 
-				console.log('New route: ' + path);
+				console.log("New route: " + path);
 				let _route = async (method, res, req) => {
-					console.log('\t' + method.toUpperCase() + ' Registered');
+					console.log("\t" + method.toUpperCase() + " Registered");
 					if (route.settings !== undefined) {
 						if (route.settings.requireLogin) {
 							let result = await isLoggedIn(req, this);
@@ -274,59 +269,57 @@ export class Server {
 						}
 
 						if (route.settings.admin && !req.session.admin)
-							return userError(res, 'Admin only route');
+							return userError(res, "Admin only route");
 
 						if (
 							route.settings.requireTicket &&
-							!res.session.role !== 'TICKET_HOLDER' &&
+							!res.session.role !== "TICKET_HOLDER" &&
 							!res.session.admin
 						) {
-							return userError(res, 'Ticket Holder only route');
+							return userError(res, "Ticket Holder only route");
 						}
 					}
 
 					try {
 						await route[method](req, res);
 					} catch (error) {
-						console.log(
-							`Error in ${method.toUpperCase()} route: ` + path
-						);
+						console.log(`Error in ${method.toUpperCase()} route: ` + path);
 						console.error(error);
-						return userError(res, 'Internal Server Error');
+						return userError(res, "Internal Server Error");
 					}
 				};
 
 				if (route.settings) {
 					if (route.settings.requireLogin) {
-						console.log('\tLogin Required');
+						console.log("\tLogin Required");
 					}
 
 					if (route.settings.admin) {
-						console.log('\tAdmin Only');
+						console.log("\tAdmin Only");
 					}
 				}
 
 				if (route.default) {
-					console.log('\tMiddleware Registered');
+					console.log("\tMiddleware Registered");
 					this.app.use(route.default);
 				}
 
 				if (route.post) {
-					console.log('\tPost Registered');
+					console.log("\tPost Registered");
 					this.app.post(path, async (req, res) => {
-						await _route('post', res, req);
+						await _route("post", res, req);
 					});
 				}
 
 				if (route.get) {
-					console.log('\tGet Registered');
+					console.log("\tGet Registered");
 					this.app.get(path, async (req, res) => {
-						await _route('get', res, req);
+						await _route("get", res, req);
 					});
 				}
 
 				this.routes.push({ ...route, path: path });
-			})
+			}),
 		);
 	}
 }
@@ -336,20 +329,20 @@ export default server;
 
 (async () => {
 	let startExpress = true;
-	if (argv['noExpress']) startExpress = false;
+	if (argv["noExpress"]) startExpress = false;
 	await server.start(startExpress);
 
-	if (argv['worker']) {
+	if (argv["worker"]) {
 		let result = await import(
-			'./workers/' +
-				(argv['worker'].indexOf('.mjs') !== -1
-					? argv['worker']
-					: argv['worker'] + '.mjs')
+			"./workers/" +
+				(argv["worker"].indexOf(".mjs") !== -1
+					? argv["worker"]
+					: argv["worker"] + ".mjs")
 		);
 
 		if (result.run) {
 			await result.run(server);
-		} else console.error('Worker does not have a run function');
+		} else console.error("Worker does not have a run function");
 
 		process.exit(0);
 	}
--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,101 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Web3ContextProvider from './contexts/web3Context';
-import ENSContextProvider from './contexts/ensContext';
-import ClientContextProvider from 'infinitymint-client/dist/src/contexts/clientProvider';
-import config from './infinitymint.client.config';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Web3ContextProvider from "./contexts/web3Context";
+import ENSContextProvider from "./contexts/ensContext";
 
-import IDE from './pages/IDE';
-import Index from './pages/Index';
-import Viewer from './pages/Viewer';
-import NotFound from './pages/NotFound';
-import LoginContextProvider from './contexts/loginContext';
-import Converter from './pages/utilities/Converter';
-import ContentHash from './pages/utilities/ContentHash';
-import NameHash from './pages/utilities/NameHash';
-import Dashboard from './pages/utilities/Dashboard';
-import ENSLookup from './pages/utilities/ENSLookup';
-import User from './pages/utilities/User';
-import Logout from './pages/utilities/Logout';
-import Properties from './pages/Properties';
-import Leaderboard from './pages/Leaderboard';
-import HistoryCapture from './components/HistoryCapture';
-import History from './pages/History';
-import VirtualRegistry from './pages/VirtualRegistry';
-import ENS from './pages/ENS';
-import EnsClub from './pages/onboard/EnsClub';
+import IDE from "./pages/IDE";
+import Index from "./pages/Index";
+import Viewer from "./pages/Viewer";
+import NotFound from "./pages/NotFound";
+import LoginContextProvider from "./contexts/loginContext";
+import Converter from "./pages/utilities/Converter";
+import ContentHash from "./pages/utilities/ContentHash";
+import NameHash from "./pages/utilities/NameHash";
+import Dashboard from "./pages/utilities/Dashboard";
+import ENSLookup from "./pages/utilities/ENSLookup";
+import User from "./pages/utilities/User";
+import Logout from "./pages/utilities/Logout";
+import Properties from "./pages/Properties";
+import Leaderboard from "./pages/Leaderboard";
+import HistoryCapture from "./components/HistoryCapture";
+import History from "./pages/History";
+import VirtualRegistry from "./pages/VirtualRegistry";
+import ENS from "./pages/ENS";
+import EnsClub from "./pages/onboard/EnsClub";
 function App() {
 	return (
-		<ClientContextProvider config={config as any}>
-			<Web3ContextProvider>
-				<LoginContextProvider>
-					<ENSContextProvider>
-						<BrowserRouter>
-							<HistoryCapture>
-								<Switch>
-									<Route path="/ide">
-										<IDE />
-									</Route>
-									<Route exact path="/">
-										<Index />
-									</Route>
-									<Route exact path="/index">
-										<Index />
-									</Route>
-									<Route path="/view/:token">
-										<Viewer />
-									</Route>
-									<Route path="/viewer/:token">
-										<Viewer />
-									</Route>
-									<Route path="/🧱/:token">
-										<Viewer />
-									</Route>
-									<Route path="/registry/:domain">
-										<VirtualRegistry />
-									</Route>
-									<Route path="/ens/:domain">
-										<ENS />
-									</Route>
-									<Route exact path="/utilities/converter">
-										<Converter />
-									</Route>
-									<Route exact path="/utilities/contenthash">
-										<ContentHash />
-									</Route>
-									<Route exact path="/utilities/ens">
-										<ENSLookup />
-									</Route>
-									<Route exact path="/utilities/">
-										<Dashboard />
-									</Route>
-									<Route exact path="/utilities/namehash">
-										<NameHash />
-									</Route>
-									<Route exact path="/utilities/user">
-										<User />
-									</Route>
-									<Route exact path="/utilities/logout">
-										<Logout />
-									</Route>
-									<Route exact path="/properties">
-										<Properties />
-									</Route>
-									<Route exact path="/leaderboard/top">
-										<Leaderboard />
-									</Route>
-									<Route exact path="/history">
-										<History />
-									</Route>
-									<Route exact path="/onboard/10kclub">
-										<EnsClub />
-									</Route>
-									<Route exact path="*">
-										<NotFound />
-									</Route>
-								</Switch>
-							</HistoryCapture>
-						</BrowserRouter>
-					</ENSContextProvider>
-				</LoginContextProvider>
-			</Web3ContextProvider>
-		</ClientContextProvider>
+		<Web3ContextProvider>
+			<LoginContextProvider>
+				<ENSContextProvider>
+					<BrowserRouter>
+						<HistoryCapture>
+							<Switch>
+								<Route path="/ide">
+									<IDE />
+								</Route>
+								<Route exact path="/">
+									<Index />
+								</Route>
+								<Route exact path="/index">
+									<Index />
+								</Route>
+								<Route path="/view/:token">
+									<Viewer />
+								</Route>
+								<Route path="/viewer/:token">
+									<Viewer />
+								</Route>
+								<Route path="/🧱/:token">
+									<Viewer />
+								</Route>
+								<Route path="/registry/:domain">
+									<VirtualRegistry />
+								</Route>
+								<Route path="/ens/:domain">
+									<ENS />
+								</Route>
+								<Route exact path="/utilities/converter">
+									<Converter />
+								</Route>
+								<Route exact path="/utilities/contenthash">
+									<ContentHash />
+								</Route>
+								<Route exact path="/utilities/ens">
+									<ENSLookup />
+								</Route>
+								<Route exact path="/utilities/">
+									<Dashboard />
+								</Route>
+								<Route exact path="/utilities/namehash">
+									<NameHash />
+								</Route>
+								<Route exact path="/utilities/user">
+									<User />
+								</Route>
+								<Route exact path="/utilities/logout">
+									<Logout />
+								</Route>
+								<Route exact path="/properties">
+									<Properties />
+								</Route>
+								<Route exact path="/leaderboard/top">
+									<Leaderboard />
+								</Route>
+								<Route exact path="/history">
+									<History />
+								</Route>
+								<Route exact path="/onboard/10kclub">
+									<EnsClub />
+								</Route>
+								<Route exact path="*">
+									<NotFound />
+								</Route>
+							</Switch>
+						</HistoryCapture>
+					</BrowserRouter>
+				</ENSContextProvider>
+			</LoginContextProvider>
+		</Web3ContextProvider>
 	);
 }
 
